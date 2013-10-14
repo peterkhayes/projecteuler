95dfb22ecc60f3ccd0dd3a8ac6c653659f132e6b
Peter Hayes <peter.k.hayes@gmail.com>
2013-10-14 07:27:31 -0700

    Solving problem 50 and cleanup of directory

---
+++ Javascript/src/50 - Consecutive Prime Sum.js
@@ -0,0 +1,34 @@
+var cps = function (n) {
+    var primes = [false, false];
+    for (var i = 2; i < n; i++) {
+        if (primes[i] === undefined) {
+            primes[i] = true;
+            for (var j = 2*i; j < n; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+    
+    var sum = 0;
+    var length = 0;
+    
+    var maxSum = 0;
+    var maxLength = 1;
+    
+    for (j = 2; j < n; j++) {
+        if (!primes[j]) continue;
+        sum = 0;
+        length = 0;
+        for (i = j; i < n/21; i++) {
+            if (!primes[i]) continue;
+            sum += i;
+            length++;
+            if (primes[sum] && (length > maxLength)) {
+                maxLength = length;
+                maxSum = sum;    
+            }
+        }
+    }
+    
+    return [maxSum, maxLength];
+};
